8fb3a53aa975d38aec9a2ac2f12f4b173ae4c45f
richie <teorichiemalaya@gmail.com>
2022-11-15 11:32:36 +0800

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { install } from "resize-observer";
+
+if (!window.ResizeObserver) install();
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import Header from '../components/Header'
-import Chart from '../components/Chart'
+import CandleChart from '../components/Chart'
+
 
 const Home = () => {
+    const [dimensions, setDimensions] = useState({
+      width: window.innerWidth,
+    });
+
+     useEffect(() => {
+       function handleResize() {
+         setDimensions({
+           width: window.innerWidth,
+         });
+       }
+       window.addEventListener("resize", handleResize);
+       return (_) => {
+         window.removeEventListener("resize", handleResize);
+       };
+     });
+
+     console.log(dimensions.width);
   return (
     <React.Fragment>
-      <main>
+      <main style={{ backgroundColor: "#17182c" }}>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4 text-center text-gray-400">
-              
+            <div className="grid grid-cols-9">
+              <div className="col-span-6 my-2">
+                <CandleChart width={dimensions.width} height={dimensions.height}/>
+              </div>
+              <div className="col-span-3"></div>
             </div>
           </div>
           {/* /End replace */}
