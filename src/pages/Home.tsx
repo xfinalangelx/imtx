4b4f0106e76ad5152eccac2494c3bf71aa0a3ba5
richie <teorichiemalaya@gmail.com>
2022-11-17 11:26:27 +0800

    added chart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import CandleChart from '../components/Chart'
 import PriceBar from '../components/PriceBar'
 import TradeNav from '../components/TradeNav'
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
 
 const Home = () => {
@@ -29,9 +30,19 @@ const Home = () => {
         <div className="max-w-7xl mx-auto py-2 sm:px-6 lg:px-8">
           <PriceBar />
           <div className="px-4 py-6 sm:px-0">
-            <div className="grid grid-cols-9">
-              <div className="col-span-6 my-2">
-                <CandleChart width={dimensions.width} height={dimensions.height}/>
+            <div className="grid grid-cols-10">
+              <div className="col-span-7 my-2">
+                {/*<CandleChart width={dimensions.width} height={dimensions.height}/>*/}
+                <div style={{ float: "left", width: "100%", height: "50vh" }}>
+                  <TradingViewWidget
+                    symbol="BINANCE:BTCUSDT"
+                    interval="15"
+                    timezone="Etc/UTC"
+                    theme={Themes.DARK}
+                    locale="en"
+                    autosize
+                  />
+                </div>
               </div>
               <div className="col-span-3"></div>
             </div>
